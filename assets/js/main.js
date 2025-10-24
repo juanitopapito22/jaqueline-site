e06248073de378d336f15a383fb12f29bac9e1e3
juanitopapito22 <juanpablo.000@hotmail.es>
2025-10-24 12:42:44 +0200

    actualizacion celular

--- assets/js/main.js
+++ assets/js/main.js
@@ -59,7 +59,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const tbH = Math.ceil(topbar.getBoundingClientRect().height || 0);
 
     // 🎯 Compensamos más en pantallas pequeñas, pero mantenemos sensible en desktop
-    const vhCompensate = window.innerWidth < 768 ? 120 : 5;
+    const vhCompensate = window.innerWidth < 768 ? 140 : 5;
     const margin = `-${tbH + vhCompensate}px 0px 0px 0px`;
 
     observer = new IntersectionObserver(([entry]) => {
